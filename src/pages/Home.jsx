f7911f4408ba8540b821cf46d14cb581fbe1183d
unknown <Mandar2107>
2025-02-10 19:46:25 +0530

    worken on src

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <img src="/images/Mandar photo.JPG" alt="Profile" className="profile-img" />
+      <h1>Welcome to My Portfolio</h1>
+      <p>Aspiring Software Engineer | Passionate about Technology</p>
+      <button className="button">View My Work</button>
+    </div>
+  );
+};
+
+export default Home;
